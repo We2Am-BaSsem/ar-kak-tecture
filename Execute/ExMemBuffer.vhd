a98415bb7dcf9113c57279c1a9e4879b59562cd4
We2Am-BaSsem <58189568+We2Am-BaSsem@users.noreply.github.com>
2021-12-31 18:37:16 +0200

    Update ExMemBuffer.vhd

--- Execute/ExMemBuffer.vhd
+++ Execute/ExMemBuffer.vhd
@@ -1,15 +1,14 @@
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
-
-
 ENTITY ex_mem_buffer IS
-    GENERIC (n : INTEGER := 32);
     PORT (
-        D : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0) := (OTHERS => '0');
-        Q : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0) := (OTHERS => '0');
+        D : IN STD_LOGIC_VECTOR(128 DOWNTO 0) := (OTHERS => '0');
+        Q : OUT STD_LOGIC_VECTOR(128 DOWNTO 0) := (OTHERS => '0');
         clk, rst, en : IN STD_LOGIC := '0'
     );
-    --memtoreg, memwrite,memread,pop,push,fnjump, ALUout, DataToStore(D1), PC
+    --  memtoreg(1bit), memwrite(1bit),memread(1bit),pop(1bit),push(1bit),fnjump(1bit), => 6 bits
+    --  ALUout(16bit), DataToStore(D1)(16bit), PC(32bit) => 64bits
+    --  Used 70bits
 END ENTITY;
 
 ARCHITECTURE nbits_register OF my_register IS
@@ -19,7 +18,7 @@ BEGIN
         IF en = '1' THEN
             IF rst = '1' THEN
                 Q <= (OTHERS => '0');
-            ELSIF rising_edge(clk) THEN
+            ELSIF falling_edge(clk) THEN
                 Q <= D;
             END IF;
         END IF;
